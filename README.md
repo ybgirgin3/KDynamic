a1f81d5fe25c83b5e0dd28db67279a07a353fd63
Yusuf Berkay Girgin <35403526+ybgirgin3@users.noreply.github.com>
2019-09-05 11:10:14 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,61 @@
+# KDynamic: An Alternative to MacOS Mojave Dynamic Theme and Wallpaper Changer for KDE Plasma 5
+
+## Description
+
+KDynamic is a multiple python script which makes whole system theme light or dark by specified hours
+
+KDynamic uses native KDE Desktop Environment terminal commands so nothing unfamiliar
+
+## Configuration
+
+KDynamic need some paths;
+- Where your favorite light plasma theme located
+- Where your favorite dark plasma theme located
+- Where your favorite wallpapers which categorized by you
+
+
+but don't worry setup.py will make this boring stuff for you
+
+```sh
+~$ python3 setup.py
+```
+
+## What you need to do for make program run ?
+
+- First things first you need to pick up some wallpapers for each of theme maybe from [unsplash.com](http://unsplash.com) or somewhere you love
+
+- Move images to the specified folders
+
+- Make needed changes into the txt file
+
+
+## ...And what to do for make program run at startup automatically ?
+
+In KDE Plasma you need to go autostart directory
+
+```sh
+~$ cd ~/.config/autostart
+```
+
+In this folder you need to move the file which named ***kdynamic.desktop*** and of course you need to change some values in it
+
+
+After all things done you need to make 'main.py' file executable by;
+
+
+```sh
+~$ sudo chmod +x main.py
+```
+command and when you restart your system program will run at startup
+
+
+**This script created with python3**
+
+
+**Script includes print functions so you can see what's going on while this little script running if you want**
+
+for this run program manuelly;
+
+```sh
+~$ python3 main.py
+```
---
+++ setup.py
@@ -0,0 +1,87 @@
+# !/usr/bin/python3
+import os
+from time import sleep
+
+homedir = os.path.expanduser('~')
+
+
+###### light_plasma txtleri içinde path oluşuturucu koy
+
+def txt_creator(theme):
+    """
+        txt_creator('light_plasma_theme.txt')
+        txt_creator('night_plasma_theme.txt')
+    """
+    with open(theme, 'w') as f:
+        if theme == 'light_plasma_theme.txt' or theme == 'night_plasma_theme.txt':
+            f.write('enter your favorite theme name here (only name of it not path !! )')
+
+        else:
+            f.write('# replace here with this command \nqdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript \'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///home/<username>/<theme_file_path>/<image_name>.jpg")}\'\n within filling needed spaces.. and of course for each of pics.. :(')
+
+def installer():
+    print('! Please wait until process end... !')
+    sleep(1)
+    print('[-] home directory detecting..')
+    sleep(2)
+    print('[+] done..')
+    sleep(.5)
+    ### create wallpapers directory
+    ## light theme wallpapers
+    # go to home directory
+    print('[-] directory changing to home directory..')
+    os.chdir(homedir)
+    sleep(1)
+    print('[+] done..')
+    sleep(.5)
+    # create files for light theme
+    print('[+] done..')
+    sleep(.5)
+    print('[-] KDynamic files creating for light theme wallpapers..')
+    os.makedirs('{}/KDynamic/Pictures/light/'.format(homedir))
+    sleep(1)
+    print('[+] done..')
+    sleep(.5)
+    print('txt files creating for plasma themes..')
+    os.chdir('{}/KDynamic/'.format(homedir))
+    txt_creator('light_plasma_theme.txt')
+    txt_creator('night_plasma_theme.txt')
+    sleep(1)
+    print('[+] done..')
+    sleep(.5)
+    print('[-] light.txt file creating..')
+    os.chdir('{}/KDynamic/Pictures/light'.format(homedir))
+    txt_creator('light.txt')
+    sleep(1)
+    print('[+] done..')
+    sleep(1)
+    # get into KDynamic/Pictures file
+    print('[-] KDynamic files creating for light_dark theme wallpapers..')
+    os.chdir('{}/KDynamic/Pictures'.format(homedir))
+    sleep(1)
+    # create file for light dark theme
+    os.makedirs('light_dark')
+    sleep(1)
+    print('[-] light_dark.txt file creating..')
+    os.chdir('{}/KDynamic/Pictures/light_dark'.format(homedir))
+    txt_creator('light_dark.txt')
+    sleep(1)
+    print('[+] done..')
+    sleep(.5)
+    print('[-] KDynamic files creating for night theme wallpapers')
+    # create file for night theme
+    os.chdir('{}/KDynamic/Pictures/'.format(homedir))
+    os.makedirs('night')
+    sleep(1)
+    print('[+] done..')
+    sleep(1)
+    print('[-] night.txt file creating..')
+    os.chdir('{}/KDynamic/Pictures/night'.format(homedir))
+    txt_creator('night.txt')
+    sleep(1)
+    print('[+] done..')
+    print('[+] all done..')
+    print('Files located in {}/KDynamic/'.format(homedir))
+
+if __name__ == '__main__':
+    installer()
