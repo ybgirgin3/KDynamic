d4aed0a0589bd9a2f09274131656c4c36e373ec3
ybgirgin3 <ybgirgin3@gmail.com>
2021-07-10 20:53:29 +0300

    asd

--- gtk_scheduler.py
+++ gtk_scheduler.py
@@ -18,18 +18,14 @@ cron = CronTab(user=user)
 
 ## get current path)
 command = f"/usr/bin/python3 {os.getcwd()}/theme_cfg/tmain.py"
+comment = 'KDynamic: An Alternative to MacOS Mojave Dynamic Theme and Wallpaper Changer - GNOME'
 # print(command)
 
 ## creating job is done
 def creating_cron_job():
     ## create job
-    job = cron.new(command=command, comment = 'KDynamic: An Alternative to MacOS Mojave Dynamic Theme and Wallpaper Changer - GNOME')
+    job = cron.new(command=command, comment = comment)
 
-    ## çalışmıyor
-    # job = cron.new(command = 'python3 /home/berkay/MEGA/kod/PYTHON/Kendi_Denemelerim/Python_ile_Uygulama_Çalışmaları/Python_Masaüstü_Uygulama_Prototipi_Serisi/PythonProjectsSeries/ThemeChanger/0.3/cron_try/hello.py', comment= 'deneme')
-
-    ## bu alttaki komut çalışıyor
-    # job = cron.new(command = 'touch /tmp/foobar123', comment= 'deneme')
 
     ### ne kadar sürede bir olacak ???
     # job.minute.every(1)
@@ -43,16 +39,18 @@ def creating_cron_job():
 ## controlling if a job existed or not with same name
 # if existed disable it and create new one
 def controlling_job():
-    a = cron.find_comment(command)
-    ## works
-    if a:
-        print('schedule already created.. removing..')
-        cron.remove(a)
-        print('re-creating')
-        creating_cron_job()
+    for c in cron:
+        if c.comment == comment:
+            cron.remove(c)
+    # a = cron.find_comment(command)
+    # ## works
+    # if a:
+    #     print('schedule already created.. removing..')
+    #     cron.remove(a)
+    #     print('re-creating')
+    #     creating_cron_job()
 
-    if not a:
-        creating_cron_job()
+    creating_cron_job()
 
 
 
