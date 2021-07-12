8b1b7780be4e21f71027ee68db2d863ce1758fde
ybgirgin3 <ybgirgin3@gmail.com>
2021-07-12 11:11:12 +0300

    asd

--- utils/utils.py
+++ utils/utils.py
@@ -32,11 +32,7 @@ def wallp_change(env: str, data: str) -> None:
         cmd = """
         dbus-send --session --dest=org.kde.plasmashell --type=method_call /PlasmaShell org.kde.PlasmaShell.evaluateScript 'string: 
         var Desktops = desktops(); 
-        for (i=0;i<Desktops.length;i++){	
-            d = Desktops[i]; d.wallpaperPlugin = "org.kde.image"; 
-            d.currentConfigGroup = Array("Wallpaper","org.kde.image","General"); 
-            d.writeConfig("Image","file:///{}");
-        }'""".format(data)
+        for (i=0;i<Desktops.length;i++){d = Desktops[i]; d.wallpaperPlugin = "org.kde.image"; d.currentConfigGroup = Array("Wallpaper","org.kde.image","General"); d.writeConfig("Image","file:///{}");}'""".format(data)
         
         
 
